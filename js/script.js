d711b347f3d684c99c3d25acb8a0ece595fb6511
Pilar <pilarnaviocolon@gmail.com>
2021-10-30 18:36:26 +0200

    avance en funciones de validacion del email y funcion cálculo precio

--- js/script.js
+++ js/script.js
@@ -1,49 +1,95 @@
-//============= Validación del input EMAIL =============
+
 
 window.onload = function(){
-    //ConText5.onblur = validacionClienteCorreo() //Solo aceptamos cuentas de Gmail -------NO FUNCIONA
-    ConText5.onblur = caracteresMin //Tiene que tener más de 10 caracteres
-    ConText5.onsubmit = sinEspacios() //Eliminar espacios
-   // ConText5.onsubmit = cumplimentado() //Que el campo no esté vacío al hacer clic en Enviar
+
+    //============= Validación del input EMAIL =============
+    email.onsubmit = sinEspacios //Eliminar espacios
+    email.onblur = caracteresMin //Tiene que tener más de 10 caracteres
+    email.onsubmit = validaEmail //¿Contiene una @?
+    //email.onblur = validacionClienteCorreo() //Solo aceptamos cuentas de Gmail -------NO FUNCIONA
+   // email.onsubmit = cumplimentado() //Que el campo no esté vacío al hacer clic en Enviar
+   
+   var validacionEnvio;
     
 
 
-    //funcion que limita las cuentas de correo de un determinado cliente -------NO FUNCIONA
-   document.getElementById("ConText5").onclick = function(){
-    
-    function validacionClienteCorreo() {
-        var dominioValido = "gmail.com";
-        var stringEmail = document.getElementById("ConText5").value;
-        console.log(stringEmail);
-        var separador = "@";
-        var dominioIntroducido = stringEmail.split(separador);
-        console.log(dominioIntroducido);
-        if (dominioIntroducido!==dominioValido){
-            alert("Solo aceptamos cuentas de Gmail");
+   document.getElementById("email").onclick = function(){
+
+        //¿Como le devuelvo el valor de inputSinEspacios al valor del email?
+        //funcion que devuelve el valor del input del email sin espacios
+        function sinEspacios(){
+            var inputConEspacios= document.getElementById("email").value;
+            var inputSinEspacios = inputConEspacios.trim();
+            var email = inputSinEspacios;
+            return(email);
         }
-        return dominioValido.toLowerCase() === dominioValido.toLowerCase()
+
+        //funcion que limita las cuentas de correo de un determinado cliente -------NO FUNCIONA
+        function validacionClienteCorreo() {
+            var dominioValido = "gmail.com";
+            var stringEmail = document.getElementById("email").value;
+            console.log(stringEmail);
+            var separador = "@";
+            var dominioIntroducido = stringEmail.split(separador);
+            console.log(dominioIntroducido);
+            if (dominioIntroducido.toLowerCase()!==dominioValido.toLowerCase()){
+                alert("Solo aceptamos cuentas de Gmail");
+                //email.innerHTML += "<p>Solo aceptamos cuentas de Gmail</p>" //El párrafo se añade después del imput email
+                validacionEnvio= "false";
+            }
+            
+        }
         
+
+        
+
+        //funcion que establece el numero de caracteres mínimos
+        function caracteresMin() {
+            var minimoCaracteres = ("@gmail.com").length;
+            if(email.value.length <= minimoCaracteres)
+                //document.write('<p>Cuenta de correo demasiado corta'); --------BORRAR: lo imprime en otra página
+                alert("Cuenta de correo demasiado corta");
+                validacionEnvio= "false";
         }
+
+        function validaEmail(){
+            var inputEmail= document.getElementById("email").value.indexOf("@");
+            if (inputEmail== -1){
+                alert("No es un formato compatible con una dirección de correo");
+                validacionEnvio= "false";
+            }  
+
+        }
+
+
+
     }
 
-    
+    //============= CÁLCULO PRECIO =============
+    calcularPrecio() 
 
-    //funcion que establece el numero de caracteres mínimos
-    function caracteresMin() {
-        var minimoCaracteres = ("@gmail.com").length;
-        if(ConText5.value.length <= minimoCaracteres)
-            //document.write('<p>Cuenta de correo demasiado corta'); --------BORRAR: lo imprime en otra página
-            alert("Cuenta de correo demasiado corta");
-    }
+        var precio= 0;
+        switch(document.getElementById("rbtam")) {
+            case pequeña :
+                precio+=5;
+                break;
+            case mediana :
+                precio+=10;
+                break;
+            case familiar :
+                precio+=15;
+                break;
+        }
+        for (let i=1; i<=5; i++){
+            let id= "cbox" + i;
+            if(document.getElementById(id)== checked)
+                precio += 1;
+        }
+        console.log(precio);
 
 
-    //¿Como le devuelvo el valor de inputSinEspacios al valor del ConText5?
-    function sinEspacios(){
-        var inputConEspacios= document.getElementById("ConText5").value;
-        var inputSinEspacios = inputConEspacios.trim();
-        return(inputSinEspacios);
-    }
 
 }
+
 
 
